805bb374c9ec4100714d160e8846d98cb417366c
nasseralikarimi <nasser.karimi@cloudelligent.com>
2023-03-06 16:12:30 +0430

    comission fee calulation applied for private and business model

--- src/Service/Math.php
+++ src/Service/Math.php
@@ -7,6 +7,7 @@ namespace Karimi\CommissionTask\Service;
 class Math
 {
     private $scale;
+    private $withdrawPerWeek;
 
     public function __construct(int $scale)
     {
@@ -24,13 +25,13 @@ class Math
     public function commissionFeeCalc()
     {
         $fees = [];
-        $file = fopen(__DIR__.'/../../input.csv', 'r');
+        $file = fopen(__DIR__ . '/../../input.csv', 'r');
         while (($transaction = fgetcsv($file)) !== false) {
             // Check which type of transaction it is? deposit or withdraw.
             if ($transaction[3] === 'deposit') {
-                $fees[] = $this->deposit($transaction);
+                $fees[] = $this->feeFormater($this->deposit($transaction));
             } else {
-                $fees[] = $this->withdraw($transaction);
+                $fees[] = $this->feeFormater($this->withdraw($transaction));
             }
         }
         var_dump($fees);
@@ -50,16 +51,73 @@ class Math
     {
         // There are different calculation rules for withdraw of private and business clients.
         $amount = $transaction[4];
-        if ($transaction[2] === 'private') {
-            // Private Clients, Commission fee - 0.3% from withdrawn amount.
-            // 1000.00 EUR for a week (from Monday to Sunday) is free of charge
+        $rate = $this->checkRate($transaction);
 
-            $commissionFee = $amount * 0.3 / 100;
+        if ($transaction[2] === 'private') {
+            $chargeStatus = $this->freeCharge($transaction);
+            if ($chargeStatus['isFree']) {
+                $commissionFee = 0;
+            } else {
+                // Private Clients, Commission fee - 0.3% from withdrawn amount.
+                // 1000.00 EUR for a week (from Monday to Sunday) is free of charge
+                $commissionFee = ($chargeStatus['amount'] / $rate) * 0.3 / 100;
+            }
         } else {
             // Business Clients, Commission fee - 0.5% from withdrawn amount.
-            $commissionFee = $amount * 0.5 / 100;
+            $commissionFee = ($amount / $rate) * 0.5 / 100;
         }
 
         return $commissionFee;
+    }
+
+    public function checkRate($transaction)
+    {
+        $rate = 1;
+        if ($transaction[5] != "EUR") {
+            if ($transaction[5] == "JPY") {
+                $rate = 129.53;
+            } else {
+                $rate = 1.1497;
+            }
+        }
+        return $rate;
+    }
+    public function freeCharge($transaction)
+    {
+        $year = date('Y', strtotime($transaction[0]));
+        $week = date('W', strtotime($transaction[0]));
+        $rate = $this->checkRate($transaction);
+
+        // $oldAmount = $this->withdrawPerWeek[$week][$transaction[1]];
+        if (
+            is_array($this->withdrawPerWeek) && array_key_exists($week, $this->withdrawPerWeek) &&
+            is_array($this->withdrawPerWeek[$week]) && array_key_exists($transaction[1], $this->withdrawPerWeek[$week])
+        ) {
+            $oldAmount = array_sum($this->withdrawPerWeek[$week][$transaction[1]]);
+            if (count($this->withdrawPerWeek[$week][$transaction[1]]) > 3) {
+                return ["isFree" => false, "amount" => $transaction[4]];
+            }
+            // $this->withdrawPerWeek[$week][$transaction[1]] = $oldAmount + $transaction[4];
+        } else {
+            $oldAmount = 0;
+        }
+
+        $this->withdrawPerWeek[$week][$transaction[1]][] = $transaction[4] / $rate;
+        $newAmount = array_sum($this->withdrawPerWeek[$week][$transaction[1]]);
+        if ($newAmount > 1000) {
+            if ($oldAmount >= 1000) {
+                $exceeded = $transaction[4];
+            } else {
+                $exceeded = $newAmount - 1000;
+            }
+
+            return ["isFree" => false, "amount" => $exceeded];
+        } else {
+            return ["isFree" => true];
+        }
+    }
+    public function feeFormater($fee)
+    {
+        return number_format((float)$fee, 2, '.', '');
     }
 }
